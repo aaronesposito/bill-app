94f307ad6135d5a961b36d1ec2bad2db138160be
aaron <esposito.aaron93@gmail.com>
2025-11-10 13:45:53 -0500

    changed vite server config

--- bill-frontend/vite.config.js
+++ bill-frontend/vite.config.js
@@ -7,15 +7,6 @@ export default defineConfig({
     host: true,          // listens on 0.0.0.0 in the container
     port: 5004,
     strictPort: true,
-    watch: {
-      usePolling: true,  // match CHOKIDAR_USEPOLLING
-      interval: 100      // tweak if needed
-    },
-    hmr: {
-      host: 'localhost', // what your browser uses to reach the app
-      port: 5004
-      // If you access via http://127.0.0.1:5004, set host: '127.0.0.1'
-    }
   },
   css: { modules: { localsConvention: 'camelCaseOnly' } }
 })
