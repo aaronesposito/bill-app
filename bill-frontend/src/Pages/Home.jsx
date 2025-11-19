799c53d77452c0e3b46cf0ca8f2cd133b06a61b3
aaron <esposito.aaron93@gmail.com>
2025-11-19 13:06:42 -0500

    removed debugging print

--- bill-frontend/src/Pages/Home.jsx
+++ bill-frontend/src/Pages/Home.jsx
@@ -1,12 +1,10 @@
 import scrooge from '../assets/scrooge.jpg'
 import styles from '../styles/Home.module.css'
-import { API_BASE_URL } from '../Config/apiConfig/';
 
 
 
 function Home() {
 
-    console.log('API_BASE_URL at runtime:', API_BASE_URL);
     return(
         <>
             <div className={styles.innerShadowBox}>
